8c5e852edd54797ecee63a0400611bd1245437e3
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2018-03-30 12:08:48 -0700

    Add callsite tracing option.

--- pytils/log.py
+++ pytils/log.py
@@ -6,9 +6,10 @@ import sys
 
 USER = "user"
 user_log = logging.getLogger(USER)
+DEFAULT_FORMATTER = u"%(levelname)s: %(message)s"
+CALLSITE_FORMATTER = u"%(levelname)s %(module)s..%(funcName)s: %(message)s"
 
-
-def setup_logging(root_log_file, verbose):
+def setup_logging(root_log_file, verbose, callsite=False):
     logging.getLogger().setLevel(logging.DEBUG)
 
     if verbose:
@@ -18,8 +19,8 @@ def setup_logging(root_log_file, verbose):
         user_log.setLevel(logging.INFO)
         logging.getLogger().setLevel(logging.INFO)
 
-    root_handler = logging.FileHandler(root_log_file)
-    root_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+    root_handler = logging.FileHandler(root_log_file, encoding = "UTF-8")
+    root_handler.setFormatter(logging.Formatter(CALLSITE_FORMATTER if callsite else DEFAULT_FORMATTER))
     logging.getLogger().addHandler(root_handler)
 
     user_handler = logging.StreamHandler(sys.stdout)
