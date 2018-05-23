f44589a9e31ddc83da0bf17fc0724a4af182e270
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2018-05-22 19:45:05 -0700

    Implement asynchronous logging.

--- pytils/log.py
+++ pytils/log.py
@@ -3,12 +3,32 @@
 
 import logging
 from logging.handlers import RotatingFileHandler
+from queue import Queue
+from threading import Thread
 import sys
+
 
 USER = "user"
 user_log = logging.getLogger(USER)
 DEFAULT_FORMATTER = u"%(levelname)s: %(message)s"
 CALLSITE_FORMATTER = u"%(levelname)s %(module)s..%(funcName)s: %(message)s"
+
+
+class AsynchRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, *args, **kwargs):
+        super(AsynchRotatingFileHandler, self).__init__(*args, **kwargs)
+        self._queue = Queue()
+        self._thread = Thread(target=self._process)
+        self._thread.daemon = True
+        self._thread.start()
+
+    def emit(self, record):
+        self._queue.put(record)
+
+    def _process(self):
+        while True:
+            super(AsynchRotatingFileHandler, self).emit(self._queue.get())
+
 
 def setup_logging(root_log_file, verbose, callsite=False):
     logging.getLogger().setLevel(logging.DEBUG)
@@ -20,8 +40,8 @@ def setup_logging(root_log_file, verbose, callsite=False):
         user_log.setLevel(logging.INFO)
         logging.getLogger().setLevel(logging.INFO)
 
-    #                                                                  5MBs
-    root_handler = RotatingFileHandler(root_log_file, maxBytes=5*1024*1024, backupCount=5, encoding="utf-8")
+    #                                                                       5MBs
+    root_handler = AsynchRotatingFileHandler(root_log_file, maxBytes=5*1024*1024, backupCount=5, encoding="utf-8")
     root_handler.setFormatter(logging.Formatter(CALLSITE_FORMATTER if callsite else DEFAULT_FORMATTER))
     logging.getLogger().addHandler(root_handler)
 
