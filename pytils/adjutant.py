7216450cc2a303d9b3aa9bba02f1ab64fc8a4a61
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2019-03-01 16:19:53 -0800

    Improvements to dict_as_str.

--- pytils/adjutant.py
+++ pytils/adjutant.py
@@ -1,10 +1,39 @@
 
 
-def dict_as_str(d, use_key=True, reverse=False):
+def dict_as_str(d, sort_by_key=True, reverse=False, digits=4):
+    if len(d) == 0:
+        return "{}"
+
     out = []
 
-    for item in sorted(d.items(), key=lambda item: item[0] if use_key else item[1], reverse=reverse):
-        out.append("%s: %s" % (item[0], item[1]))
+    def _form(item):
+        form = "s"
+
+        if isinstance(item, int):
+            form = "d"
+        elif isinstance(item, float):
+            form = ".%df" % digits
+
+        return form
+
+    def _pre_format(item):
+        if isinstance(item, dict):
+            return dict_as_str(item, sort_by_key, reverse, digits)
+        elif isinstance(item, tuple) or isinstance(item, list) or isinstance(item, set):
+            return str(item)
+        else:
+            return item
+
+    first_key = next(iter(d.keys()))
+    first_value = d[first_key]
+
+    if isinstance(first_value, dict) and not sort_by_key:
+        raise ValueError("Cannot sort_by_value with dict values.")
+
+    template = "{:%s}: {:%s}" % (_form(first_key), _form(first_value))
+
+    for key, value in sorted(d.items(), key=lambda item: item[0] if sort_by_key else item[1], reverse=reverse):
+        out.append(template.format(_pre_format(key), _pre_format(value)))
 
     return "{%s}" % ", ".join(out)
 
--- pytils/tests/adjutant.py
+++ pytils/tests/adjutant.py
@@ -5,16 +5,44 @@ from pytils.invigilator import create_suite
 
 
 class Tests(TestCase):
-    def test_dict_as_str(self):
-        # ints
+    def test_dict_as_str_sort(self):
+        # str-int
         d = {"a": 3, "b": 1, "c": 2}
         self.assertEqual(dict_as_str(d, True, False), "{a: 3, b: 1, c: 2}")
         self.assertEqual(dict_as_str(d, False, False), "{b: 1, c: 2, a: 3}")
         self.assertEqual(dict_as_str(d, True, True), "{c: 2, b: 1, a: 3}")
         self.assertEqual(dict_as_str(d, False, True), "{a: 3, c: 2, b: 1}")
-        # strs
-        self.assertEqual(dict_as_str({"a": "jack", "b": "bob"}), "{a: jack, b: bob}")
-        self.assertEqual(dict_as_str({"a": "jack", "b": "bob"}, False), "{b: bob, a: jack}")
+
+    def test_dict_as_str_combinations(self):
+        # str-str
+        d = {"a": "jack", "b": "bob"}
+        self.assertEqual(dict_as_str(d), "{a: jack, b: bob}")
+
+        # int-float
+        d = {1: .51234, 2: -1.2}
+        self.assertEqual(dict_as_str(d), "{1: 0.5123, 2: -1.2000}")
+
+        # float-str
+        d = {.51234: "jack", -1.2: "bob"}
+        self.assertEqual(dict_as_str(d), "{-1.2000: bob, 0.5123: jack}")
+
+        # str-dict
+        d = {"a": {1: "jack", 2: "bob"}, "b": {3: "alice", 4: "jacob"}}
+        self.assertEqual(dict_as_str(d), "{a: {1: jack, 2: bob}, b: {3: alice, 4: jacob}}")
+        self.assertEqual(dict_as_str(d, reverse=True), "{b: {4: jacob, 3: alice}, a: {2: bob, 1: jack}}")
+
+        # str-tuple
+        d = {"a": (2, 3), "b": (1, 2)}
+        self.assertEqual(dict_as_str(d), "{a: (2, 3), b: (1, 2)}")
+        self.assertEqual(dict_as_str(d, False), "{b: (1, 2), a: (2, 3)}")
+
+    def test_dict_as_str_unsortable(self):
+        try:
+            dict_as_str({"a": {}}, sort_by_key=False)
+            raise AssertionError("unexpected success")
+        except ValueError as e:
+            # Expected
+            pass
 
 
 def tests():
