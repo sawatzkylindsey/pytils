44989f70c1004b5fe7ae06ca7ee3a6cb1d7880b8
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2019-05-15 17:18:44 -0700

    Ensure remaining log messages are written by requiring explicit teardown.

--- pytils/log.py
+++ pytils/log.py
@@ -11,6 +11,9 @@ import sys
 
 USER = "user"
 user_log = logging.getLogger(USER)
+"""
+A logger that outputs to standard out, suitable for user messaging.
+"""
 DEFAULT_FORMATTER = u"%(levelname)s: %(message)s"
 TIME_FORMATTER = u"%(levelname)s: %(asctime)s %(message)s"
 CALLSITE_FORMATTER = u"%(levelname)s %(module)s..%(funcName)s: %(message)s"
@@ -21,18 +24,23 @@ class AsynchRotatingFileHandler(RotatingFileHandler):
     def __init__(self, *args, **kwargs):
         super(AsynchRotatingFileHandler, self).__init__(*args, **kwargs)
         self._queue = Queue()
+        self._closed = False
         self._thread = Thread(target=self._process)
-        self._thread.daemon = True
+        # Not daemon threads to ensure that the program continues to run while there are still records that need to be logged.
+        # As a result, callers must close this handler (ex: through @teardown below).
+        self._thread.daemon = False
         self._thread.start()
 
     def emit(self, record):
         self._queue.put(record)
 
     def _process(self):
-        while True:
+        while not self._closed:
             super(AsynchRotatingFileHandler, self).emit(self._queue.get())
 
     def close(self):
+        self._closed = True
+
         while not self._queue.empty():
             super(AsynchRotatingFileHandler, self).emit(self._queue.get())
 
@@ -40,6 +48,26 @@ class AsynchRotatingFileHandler(RotatingFileHandler):
 
 
 def setup_logging(root_log_file, verbose, user_verbose, callsite=False, time=False):
+    """Initialize the pytil's logger.
+
+    .. note: be sure to use with log.teardown
+
+    >>> @teardown
+    >>> def main(..):
+    >>>     setup_logging(".%s.log" % os.path.splitext(os.path.basename(__file__))[0], aargs.verbose, False)
+    >>>     ..
+
+    :param root_log_file: the log file, commonly declared as '".%s.log" % os.path.splitext(os.path.basename(__file__))[0]'
+    :type function: str
+    :param verbose: use the DEBUG logging level in the root logger
+    :type verbose: bool
+    :param user_verbose: use the DEBUG logging level in the user_log
+    :type user_verbose: bool
+    :param callsite: configure the log formatter to include the callsite in the form 'module..function'
+    :type callsite: bool
+    :param time: configure the log formatter to include time
+    :type time: bool
+    """
     if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     else:
@@ -63,6 +91,28 @@ def setup_logging(root_log_file, verbose, user_verbose, callsite=False, time=Fal
     user_log.addHandler(user_handler)
 
 
+def teardown(function):
+    """Decorator to ensure the logging handlers are closed on exit.
+
+    .. note: be sure to use with log.setup_logging
+
+    >>> @teardown
+    >>> def main(..):
+    >>>     setup_logging(..)
+    >>>     ..
+
+    :param function: the function to decorate
+    :type function: function
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        finally:
+            _clear_handlers(logging.getLogger())
+
+    return wrapper
+
+
 def _get_formatter(callsite, time):
     if callsite and time:
         return logging.Formatter(CALLSITE_TIME_FORMATTER)
@@ -76,5 +126,6 @@ def _get_formatter(callsite, time):
 
 def _clear_handlers(logger):
     for handler in logger.handlers:
+        handler.close()
         logger.removeHandler(handler)
 
